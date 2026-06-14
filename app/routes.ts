b4a875987285422b39eff32fc309f874fd6e95f1
zhangguangxun1 <zhangguangxun1@outlook.com>
2026-06-14 12:21:33 +0800

    去除组卷部分的示例代码

--- app/routes.ts
+++ app/routes.ts
@@ -2,9 +2,4 @@ import { route, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
   route("/", "routes/tiku/index.tsx", [route(":textbookId", "routes/tiku/list.tsx"), route(":textbookId/similar", "routes/tiku/similar.tsx")]),
-  route("/test", "routes/test/index.tsx", [
-    route(":textbookId", "routes/test/list.tsx"),
-    route(":textbookId/add", "routes/test/add.tsx"),
-    route(":textbookId/:testId", "routes/test/info.tsx"),
-  ]),
 ] satisfies RouteConfig;
