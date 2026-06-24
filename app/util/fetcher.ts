9e77bff955ce579efeec024e12b0d9cbf20bfd3e
zhangguangxun1 <zhangguangxun1@outlook.com>
2026-06-24 16:50:19 +0800

    年份必填不处理特殊值

--- app/util/fetcher.ts
+++ app/util/fetcher.ts
@@ -30,7 +30,7 @@ export function usePaperList(search: PaperMetaSearch, pageNo: number) {
   if (StringValidator.isNonEmpty(search.tag)) {
     req.tag = search.tag;
   }
-  if (StringValidator.isNonEmpty(search.year) && search.year !== "不选") {
+  if (StringValidator.isNonEmpty(search.year)) {
     req.year = search.year;
   }
   if (StringValidator.isNonEmpty(search.grade) && search.grade !== "不选") {
